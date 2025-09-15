d636167746ee1184f15aa44e0426e83e4ba7aeb5
Vladislav Munkuev <v.munkuev@g.nsu.ru>
2025-09-15 12:08:42 +0700

    fixing coverage

--- Task_1_1_1/src/main/java/ru/nsu/munkuev/Sample.java
+++ Task_1_1_1/src/main/java/ru/nsu/munkuev/Sample.java
@@ -4,9 +4,6 @@ package ru.nsu.munkuev;
  * Sample class to simulate 1.1 task functionality
  */
 public class Sample {
-    public static void printHelloWorld() {
-        System.out.println("Hello world!");
-    }
 
     public static void main(String[] args) {
         //printHelloWorld();
@@ -17,4 +14,5 @@ public class Sample {
             System.out.printf("%d, ", arr[i]);
         }
     }
+    
 }
