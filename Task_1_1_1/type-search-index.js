fa32682da63bb0415417bc9882726ef632fda478
Larjee <Larjee@users.noreply.github.com>
2025-09-28 09:25:29 +0000

    Deploying to gh-pages from @ Larjee/OOP@8e26d8893c3d2f3e032ec3f0e138bace9912d520 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.munkuev","l":"HeapSort"},{"p":"ru.nsu.munkuev","l":"Sample"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.munkuev","l":"HeapSort"}];updateSearchResults();
