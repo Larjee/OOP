4b09ecee03d0855b1d17a0106c27c72c1d5acafe
Vladislav Munkuev <v.munkuev@g.nsu.ru>
2025-11-03 12:20:56 +0700

    commit

---
+++ Task_1_2_1/src/main/java/ru/nsu/munkuev/Graph.java
@@ -0,0 +1,18 @@
+package ru.nsu.munkuev;
+
+import java.util.List;
+
+public interface Graph {
+     boolean addVertex(Vertex vertex);
+     boolean removeVertex(Vertex vertex);
+
+     boolean addEdge(int from, int to);
+     boolean removeEdge(int from, int to);
+
+     List<Vertex> getVertices();
+
+     List<Integer> getParents(int vertex);
+     List<Integer> getChildren(int vertex);
+
+
+}
